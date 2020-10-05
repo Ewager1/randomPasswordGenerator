484c81a2ea8712b95aca7fbf7b5fceab4135c0c0
EWager <ethanbwager@gmail.com>
2020-10-05 11:32:31 -0400

    created prompts for character types and tracking variables for user answer

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var masterPassword = [];
+var passwordLength = 0;
+var includesLowerCase = true;
+var includesUpperCase = true;
+var includesNumerical = true;
+var includesSpecialChars = true;
 
 var lowerCaseAlphabet = [
   "a",
@@ -36,6 +41,7 @@ var upperCaseAlphabet = lowerCaseAlphabet.map((lowerCaseAlphabet) =>
   lowerCaseAlphabet.toUpperCase()
 );
 //numeric assignment
+
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //special character assignment
@@ -72,6 +78,8 @@ var specialCharacters = [
   "~",
   "/",
 ];
+
+//section: password Length
 // Write password to the #password input
 function writePassword() {
   let passwordLength = prompt(
@@ -86,6 +94,41 @@ function writePassword() {
     //error handling for answers outside paremeters
     alert("Error. Please enter number between 8 and 128");
   }
+
+  //section: charactertypes
+  //confirm uppercase
+  uppercase = confirm("Would you like uppercase characters?");
+  if (uppercase === false) {
+    includesUpperCase = false;
+  } else {
+    includesUpperCase = true;
+  }
+  //confirm lowercase
+  lowercase = confirm("Would you like lowercase characters?");
+  if (lowercase === false) {
+    includesLowerCase = false;
+  } else includesLowerCase = true;
+  //confirm numeric.
+  numeric = confirm("Would you like numeric characters?");
+  if (numeric === false) {
+    includesNumerical = false;
+  } else includesNumerical = true;
+  //confirm special
+  special = confirm("Would you like special characters?");
+  if (special === false) {
+    includesSpecialChars = false;
+  } else {
+    includesSpecialChars = true;
+  }
+
+  //error handling: at least one character type must be choseen
+  if (
+    includesLowerCase === false &&
+    includesUpperCase === false &&
+    includesNumerical === false &&
+    includesSpecialChars === false
+  )
+    alert("Error. You must choose at least one character type.");
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
