70db52376e2ef49f0809348bb12e16c4d2f58ffa
EWager <ethanbwager@gmail.com>
2020-10-06 12:05:01 -0400

    learned how to connect helper functions to event function and did so.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 var masterPassword = [];
-var passwordLength = 0;
-var includesLowerCase = true;
-var includesUpperCase = true;
-var includesNumerical = true;
-var includesSpecialChars = true;
+var generateBtn = document.querySelector("#generate");
+
 
 var lowerCaseAlphabet = [
   "a",
@@ -79,62 +75,72 @@ var specialCharacters = [
   "/",
 ];
 
-//section: password Length
-// Write password to the #password input
-function writePassword() {
-  let passwordLength = prompt(
+//section: helper function that collects user input about password data
+function getUserInput(){
+  //password Length
+  var length = prompt(
     "Choose password length from 8 to 128 characters"
   );
-
   if (
-    passwordLength < 8 ||
-    passwordLength > 128
-    //This also works for empty strings, but need an answer for inputs that are Not a Number like "chair" problem is all outputs are coming back as strings.
+    length < 8 ||
+    length > 128
+    //This spot needs error handling for inputs that are not a number
   ) {
-    //error handling for answers outside paremeters
     alert("Error. Please enter number between 8 and 128");
   }
-
-  //section: charactertypes
-  //confirm uppercase
-  uppercase = confirm("Would you like uppercase characters?");
-  if (uppercase === false) {
-    includesUpperCase = false;
-  } else {
-    includesUpperCase = true;
-  }
-  //confirm lowercase
-  lowercase = confirm("Would you like lowercase characters?");
-  if (lowercase === false) {
-    includesLowerCase = false;
-  } else includesLowerCase = true;
-  //confirm numeric.
-  numeric = confirm("Would you like numeric characters?");
-  if (numeric === false) {
-    includesNumerical = false;
-  } else includesNumerical = true;
-  //confirm special
-  special = confirm("Would you like special characters?");
-  if (special === false) {
-    includesSpecialChars = false;
-  } else {
-    includesSpecialChars = true;
-  }
+  //uppercase
+  var uppercase = confirm("Would you like uppercase characters?");
+  //lowercase
+  var lowercase = confirm("Would you like lowercase characters?");
+  //numeric.
+  var numeric = confirm("Would you like numeric characters?");
+  //special
+  var special = confirm("Would you like special characters?");
 
   //error handling: at least one character type must be choseen
   if (
-    includesLowerCase === false &&
-    includesUpperCase === false &&
-    includesNumerical === false &&
-    includesSpecialChars === false
+    lowercase === false &&
+    uppercase === false &&
+    numeric === false &&
+    special === false
   )
-    alert("Error. You must choose at least one character type.");
+    alert("Error. You must choose at least one character type.")
+    //holds all user answers for generate password
+    var userAnswers = {length}
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+     if(uppercase === true){} 
+      if(lowercase === true) {}
+      if(numeric === true) {}
+      if(special === true) {}
+     
+    
+    console.log(userAnswers)
+   return userAnswers;
 
-  passwordText.value = password;
+
+function generatePassword(){
+  //Chains functions by creating 
+  var userAnswers = getUserInput();
+  console.log(userAnswers)
+   
 }
+
+
+
+
+// Write password to the #password input
+
+ function writePassword() {
+  var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+
+  //create logic that combines character lists into master list depending on user answers
+
+} 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
